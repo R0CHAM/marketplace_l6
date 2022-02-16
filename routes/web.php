bc4fc49b591097d3100c6645fc62d558dc926a4e
Matheus R. Mattos <matheusrm@id.uff.br>
2022-02-15 22:00:36 -0300

    Manipulação de usuários com active record, mass assignment/update para entender a dinâmica do eloquent com os registros no banco de dados. Com esse commit concluo a seção 4 de 25 do curso.

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,38 @@ Route::get('/', function () {
 Route::get('/oi', function () {
     return "<h1>Olá, mundo!</h1>";
 });
+
+Route::get('/products', function () {
+    $products = \App\Product::all(); // select * from products
+
+    return $products;
+});
+
+Route::get('/users', function () {
+    //================ Active record =======================
+//    $user = new \App\User;
+//    $user->name = 'Kvothe Bloodless';
+//    $user->email = 'kvothe@university.com';
+//    $user->password = bcrypt('Denna');
+//    $user->save();
+
+//    ================ Mass assignment =======================
+//    $user = \App\User::create([
+//        'name' => 'Hermione Granger',
+//        'email' => 'hermione@hogwarts.com',
+//        'password' => bcrypt('Roney'),
+//    ]);
+
+//    ================== Mass update ======================
+//    $user = \App\User::find(22);
+//    $user->update([
+//        'email' => 'granger.h@hogwarts.com',
+//    ]);
+    return \App\User::all();
+
+//    return \App\User::where('name', 'Kvothe Bloodless')->get();
+//    return \App\User::all(); //retorna uma Collection
+
+
+
+});
