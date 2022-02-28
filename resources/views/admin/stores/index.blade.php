6295fa387d5911fedae400d3e7dcf0092f5605c4
Matheus R. Mattos <matheusrm@id.uff.br>
2022-02-28 19:43:08 -0300

    Criação do middlware para fazer impedir que um mesmo usuário possua mais de uma loja.

--- resources/views/admin/stores/index.blade.php
+++ resources/views/admin/stores/index.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <a href="{{route('admin.stores.create')}}" class="btn btn-large btn-success">Criar Loja</a>
+    @if(!$store)
+        <a href="{{route('admin.stores.create')}}" class="btn btn-large btn-success">Criar Loja</a>
+    @endif
 
     <table class="table table-striped table-hover">
         <thead>
@@ -13,25 +15,21 @@
             </tr>
         </thead>
         <tbody>
-            @foreach($stores as $store)
-                <tr>
-                    <td>{{$store->id}}</td>
-                    <td>{{$store->name}}</td>
-                    <td>{{$store->user->name}}</td>
-                    <td>
-                        <div class="btn-group">
-                            <a href="{{route('admin.stores.edit', ['store' => $store->id ])}}" class="btn btn-sm btn-secondary">EDITAR</a>
-                            <form action="{{route('admin.stores.destroy', ['store' => $store->id ])}}" method="post">
-                                @csrf
-                                @method("DELETE")
-                                <button href="" class="btn btn-sm btn-danger">REMOVER</button>
-                            </form>
-                        </div>
-                    </td>
-                </tr>
-            @endforeach
+            <tr>
+                <td>{{$store->id}}</td>
+                <td>{{$store->name}}</td>
+                <td>{{$store->user->name}}</td>
+                <td>
+                    <div class="btn-group">
+                        <a href="{{route('admin.stores.edit', ['store' => $store->id ])}}" class="btn btn-sm btn-secondary">EDITAR</a>
+                        <form action="{{route('admin.stores.destroy', ['store' => $store->id ])}}" method="post">
+                            @csrf
+                            @method("DELETE")
+                            <button class="btn btn-sm btn-danger">REMOVER</button>
+                        </form>
+                    </div>
+                </td>
+            </tr>
         </tbody>
     </table>
-
-{{$stores->links()}}
 @endsection
