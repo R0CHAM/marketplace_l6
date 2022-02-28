5fd3e167d10ecf165d4cd9cf3933183635aba8ab
Matheus R. Mattos <matheusrm@id.uff.br>
2022-02-28 19:39:37 -0300

    Adicionada validação dos dados para os formulários de criação e alteração de lojas e produtos através dos Requests específicas.

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -8,22 +8,45 @@
 
         <div class="form-group">
             <label for="">Nome Produto</label>
-            <input type="text" name="name" class="form-control">
+            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{old('name')}}">
+
+            @error('name')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="">Descrição</label>
-            <input type="text" name="description" class="form-control">
+            <input type="text" name="description" class="form-control @error('description') is-invalid @enderror" value="{{old('description')}}">
+
+            @error('description')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="">Conteúdo</label>
-            <textarea name="body" id="" cols="30" rows="10" class="form-control"></textarea>
+            <textarea name="body" id="" cols="30" rows="10" class="form-control @error('body') is-invalid @enderror" value="{{old('body')}}"></textarea>
+
+            @error('body')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="">Preço</label>
-            <input type="text" name="price" class="form-control">
+            <input type="text" name="price" class="form-control @error('price') is-invalid @enderror" value="{{old('price')}}">
+            @error('name')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
 
         <div class="form-group">
--- resources/views/admin/stores/create.blade.php
+++ resources/views/admin/stores/create.blade.php
@@ -4,25 +4,49 @@
     <h1>Criar Loja</h1>
 
     <form action="{{route('admin.stores.store')}}" method="post">
-        <input type="hidden" name="_token" value="{{csrf_token()}}">
+        @csrf
         <div class="form-group">
             <label for="">Nome loja</label>
-            <input type="text" name="name" class="form-control">
+            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{old('name')}}">
+
+            @error('name')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="">Descrição</label>
-            <input type="text" name="description" class="form-control">
+            <input type="text" name="description" class="form-control @error('description') is-invalid @enderror" value="{{old('description')}}">
+
+            @error('description')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="">Telefone</label>
-            <input type="text" name="phone" class="form-control" placeholder="+55 (DDD) 2222-2222">
+            <input type="text" name="phone"placeholder="+55 DDD 2222-2222" class="form-control  @error('phone') is-invalid @enderror" value="{{old('phone')}}">
+
+            @error('phone')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="">Celular</label>
-            <input type="text" name="mobile_phone" class="form-control" placeholder="+55 (DDD) 99999-9999">
+            <input type="text" name="mobile_phone" placeholder="+55 DDD 99999-9999" class="form-control  @error('mobile_phone') is-invalid @enderror" value="{{old('mobile_phone')}}">
+
+            @error('mobile_phone')
+            <div class="invalid-feedback">
+                {{$message}}
+            </div>
+            @enderror
         </div>
 
         <div class="form-group">
